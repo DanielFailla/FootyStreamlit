e1ecea53e74316ec962f9f5d51908dfa012adfa8
DanielFailla <115690956+DanielFailla@users.noreply.github.com>
2023-07-12 11:45:34 +1000

    Update Foot_frontend.py

--- Foot_frontend.py
+++ Foot_frontend.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 import numpy as np
 
 
@@ -101,9 +101,9 @@ combines_word = f'{word_1}{word_2}'
 with tab1:
   
    st.subheader("Goal Averages")
-   st.bar_chart(data=Team1_Risk, x='Player', y='Goals_Avg', use_container_width=True)
+   #st.bar_chart(data=Team1_Risk, x='Player', y='Goals_Avg', use_container_width=True)
    st.subheader("Disposals Average")
-   st.bar_chart(data=Team1_Risk, x='Player', y='Disposals', use_container_width=True)
+   #st.bar_chart(data=Team1_Risk, x='Player', y='Disposals', use_container_width=True)
    st.divider()
    st.subheader('     📚 Bet Slip Recomendations 📚')
    st.subheader(combines_word)
@@ -132,9 +132,9 @@ with tab1:
 with tab2:
    
    st.subheader("Goal Averages")
-   st.bar_chart(data=Team2_Risk, x='Player', y='Goals_Avg', use_container_width=True)
+   #st.bar_chart(data=Team2_Risk, x='Player', y='Goals_Avg', use_container_width=True)
    st.subheader("Disposals Average")
-   st.bar_chart(data=Team2_Risk, x='Player', y='Disposals', use_container_width=True)
+   #st.bar_chart(data=Team2_Risk, x='Player', y='Disposals', use_container_width=True)
    st.subheader('     📚 Bet Slip Recomendations 📚')
    st.subheader(combines_word)
 
